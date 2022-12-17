ff8fcf64bbfae3f73a5188e48ffc3717b535a961
IzzyBizzy <rjv2745@gmail.com>
2022-12-17 12:24:32 +1000

    Fixing Quote ID get request to handle NaNs

--- routes/quotes.js
+++ routes/quotes.js
@@ -3,13 +3,14 @@ const Quote = require('../model/Quotes');
 const verify = require('../middleware/verify');
 
 router.get('/', async (req, res) => {
-    const totalQuotes = (await Quote.where().countDocuments());
+    const totalQuotes = (await Quote.where().count().countDocuments());
     const quoteUsed = Math.floor(Math.random()*(totalQuotes-1)+1);
     const quoteToUse = await Quote.findOne({id: quoteUsed});
     res.send({quote: quoteToUse.quote});
 });
 
 router.get('/:id', async (req, res) => {
+    if(isNaN(req.params)) return res.status(400).send('Bad Request')
     const { id } = req.params
     const quoteExist = await Quote.findOne({id: id});
     if (!quoteExist) return res.status(404).send('Unknown Quote Number');
@@ -18,8 +19,8 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/newquote', verify, async (req, res) => {
-
-    const newID = (await Quote.where().countDocuments()) + 1; 
+    const newID = (await Quote.where().count().countDocuments());
+    console.log('new id is %d', newID); 
     const quote = new Quote({
         quote: req.body.quote,
         id: newID
